656c8648af6e8eafdaf94a11db2ca0a0feb71b87
Adrian Contreras <contrerasbuenoadrian@gmail.com>
2025-03-05 19:00:24 +0100

    Prueba render

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,13 +1,25 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config();
 
+let firebaseConfig;
+
+// Si estás en Render (usa la variable de entorno)
+if (process.env.FIREBASE_CONFIG) {
+  firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+} else {
+  // Si estás en local, lee firebase.json
+  firebaseConfig = JSON.parse(fs.readFileSync("firebase.json", "utf-8"));
+}
+
 initializeApp({ 
-    credential: cert('../firebase.json'),
+    credential: cert(firebaseConfig),
 });
 
 const db = getFirestore();
 
 export { db };
+
